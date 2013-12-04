ac9bb825745cf503b918cae5e5932947077725ef
Kphillycat <keithwilliamsjr@gmail.com>
2013-12-04 12:00:03 -0500

    Add formations for blinker, toad and beacon. Also ask user for type of formation to start with

--- lib/app.rb
+++ lib/app.rb
@@ -1,18 +1,60 @@
 require_relative './cell'
 require_relative './world'
+require 'debugger'
 
-da_world = World.new
+DA_WORLD = World.new
 
-da_world.board[5][5].alive = true
-da_world.board[6][5].alive = true
-da_world.board[4][5].alive = true
-#Blinker
-# da_world.board[7][5].alive = true
-# da_world.board[6][4].alive = true
-
-10.times do |x|
-	puts "World #{x+1}"
-	da_world.display
-	da_world.tick!
-	sleep(0.5)
+def blinker
+	#initialize board with blinker formation
+	DA_WORLD.board[5][5].alive = true
+	DA_WORLD.board[6][5].alive = true
+	DA_WORLD.board[4][5].alive = true
 end
+
+def toad
+	#initialize board with toad formation
+	DA_WORLD.board[4][3].alive = true
+	DA_WORLD.board[4][4].alive = true
+	DA_WORLD.board[4][5].alive = true
+	DA_WORLD.board[5][2].alive = true
+	DA_WORLD.board[5][3].alive = true
+	DA_WORLD.board[5][4].alive = true
+end
+
+def beacon
+	#initialize board with beacon formation
+	DA_WORLD.board[4][3].alive = true
+	DA_WORLD.board[4][4].alive = true
+	DA_WORLD.board[5][3].alive = true
+	DA_WORLD.board[6][6].alive = true
+	DA_WORLD.board[7][5].alive = true
+	DA_WORLD.board[7][6].alive = true
+end
+
+def run
+	10.times do |x|
+		puts "World #{x+1}"
+		DA_WORLD.display
+		DA_WORLD.tick!
+		sleep(0.5)
+	end
+end
+
+puts "Welcome to the game of life where you control the world."
+puts "Which formation would you like to see? 1 - blinker, 2 - toad, 3 - beacon"
+user_input = gets.chomp
+
+#Set up board based on formation user wants to see
+if user_input == "1"
+	blinker
+elsif user_input == "2"
+	toad
+elsif user_input == "3"
+	beacon
+else
+	puts "Wrong input. Can not compute!"
+end
+#run the program
+run
+
+	
